c5fb103824310cfc3effa1325b040633c2ef139a
Rohit-Singh-07 <150457028+Rohit-Singh-07@users.noreply.github.com>
2024-03-22 02:40:56 +0530

    commit2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,18 +8,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavScrollExample() {
   return (
-    <Navbar expand="md" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+    <Navbar expand="md" bg="dark" data-bs-theme="dark" className="bg-body-tertiary md:px-4">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#" className='md:mr-[10vw]'>चलचित्र</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
+          <Form className="d-flex gap-[2vw]  xl:mr-[8vw] lg:mr-[4vw] md:mr-[6vw]">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2 xl:w-[40vw] lg:w-[40vw] md:w-[30vw]"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="me-auto my-2 my-lg-0 xl:gap-[3vw] lg:gap-[1vw] md:gap-[1.5vw] mr-[1vw]"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
@@ -30,19 +37,13 @@ function NavScrollExample() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
             <Nav.Link href="#" disabled>
               Link
             </Nav.Link>
+            
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
